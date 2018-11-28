958e5ab0d429e41127d0e61bea478d9baf15a9f0
skhadem <soroush.khadem@colorado.edu>
2018-11-28 07:57:17 -0700

    add fuinctionality to visualize the truth 3D BB from label

--- Eval.py
+++ Eval.py
@@ -15,6 +15,63 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torchvision.models import vgg
 
+
+def get_calibration_cam_to_image(cab_f):
+    for line in open(cab_f):
+        if 'P2:' in line:
+            cam_to_img = line.strip().split(' ')
+            cam_to_img = np.asarray([float(number) for number in cam_to_img[1:]])
+            cam_to_img = np.reshape(cam_to_img, (3, 4))
+    return cam_to_img
+
+
+def plot_3d_bbox(img, label_info, calib):
+    alpha = label_info['ThetaRay']
+    # theta_ray = label_info['theta_ray']
+    box_3d = []
+    center = label_info['Location']
+    dims = label_info['Dimension']
+    # calib = label_info['Calib']
+    # cam_to_img = calib['P2']
+    cam_to_img = calib
+    rot_y = alpha / 180 * np.pi  + np.arctan(center[0]/center[2])
+    # import pdb; pdb.set_trace()
+
+    for i in [1,-1]:
+        for j in [1,-1]:
+            for k in [0,1]:
+                point = np.copy(center)
+                point[0] = center[0] + i * dims[1]/2 * np.cos(-rot_y+np.pi/2) + (j*i) * dims[2]/2 * np.cos(-rot_y)
+                point[2] = center[2] + i * dims[1]/2 * np.sin(-rot_y+np.pi/2) + (j*i) * dims[2]/2 * np.sin(-rot_y)
+                point[1] = center[1] - k * dims[0]
+
+                point = np.append(point, 1)
+                point = np.dot(cam_to_img, point)
+                point = point[:2]/point[2]
+                point = point.astype(np.int16)
+                box_3d.append(point)
+
+    front_mark = []
+    for i in range(4):
+        point_1_ = box_3d[2*i]
+        point_2_ = box_3d[2*i+1]
+        cv2.line(img, (point_1_[0], point_1_[1]), (point_2_[0], point_2_[1]), (255,0,0), 2)
+        if i == 0 or i == 3:
+            front_mark.append((point_1_[0], point_1_[1]))
+            front_mark.append((point_2_[0], point_2_[1]))
+
+    cv2.line(img, front_mark[0], front_mark[-1], (255,0,0), 2)
+    cv2.line(img, front_mark[1], front_mark[2], (255,0,0), 2)
+
+    for i in range(8):
+        point_1_ = box_3d[i]
+        point_2_ = box_3d[(i+2)%8]
+        cv2.line(img, (point_1_[0], point_1_[1]), (point_2_[0], point_2_[1]), (255,0,255), 2)
+
+    return img
+
+
+
 if __name__ == '__main__':
     store_path = os.path.abspath(os.path.dirname(__file__)) + '/models'
     if not os.path.isdir(store_path):
@@ -32,9 +89,28 @@ if __name__ == '__main__':
     alpha = config['alpha']
     w = config['w']
 
+    calib_file = os.path.abspath(os.path.dirname(__file__)) + '/Kitti/training/calib/000000.txt'
+    camera_cal = get_calibration_cam_to_image(calib_file)
+
+
     data = Dataset.ImageDataset(path + '/training')
+    img = data.GetImage(10)
+
+    info = data[10]['Label']
+
+    for item in info:
+        img = plot_3d_bbox(img, item, camera_cal)
+
+    cv2.imshow('test',img)
+    cv2.waitKey(0)
+
+    exit()
+
+
+
+
     data = Dataset.BatchDataset(data, batches, bins, mode = 'eval')
-    
+
     if len(model_lst) == 0:
         print 'No previous model found, please check it'
         exit()
@@ -72,14 +148,18 @@ if __name__ == '__main__':
         elif theta < 0: theta += (int(-theta / 360) + 1) * 360
         if Ry > 0: Ry -= int(Ry / 360) * 360
         elif Ry < 0: Ry += (int(-Ry / 360) + 1) * 360
-        
+
         theta_error = abs(Ry - theta)
         if theta_error > 180: theta_error = 360 - theta_error
         angle_error.append(theta_error)
 
         dim_error = np.mean(abs(np.array(dimGT) - dim))
         dimension_error.append(dim_error)
-        
+
+
+        print(info)
+        exit()
+
         #if i % 60 == 0:
         #    print (theta, Ry)
         #    print (dim.tolist(), dimGT)
