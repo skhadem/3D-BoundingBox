1226ec59ccc92f65f90172573136ec48fe57ff76
skhadem <soroush.khadem@colorado.edu>
2019-02-28 08:03:19 -0500

    Save and load training checkpoints correctly

--- Train.py
+++ Train.py
@@ -8,20 +8,20 @@ from torch.autograd import Variable
 from torchvision.models import vgg
 from torch.utils import data
 
+
 import os
 
 def main():
 
     # hyper parameters
-    epochs = 10
+    epochs = 100
     batch_size = 8
-    alpha = 1
-    w = 1
+    alpha = 0.6
+    w = 0.4
 
 
     train_path = os.path.abspath(os.path.dirname(__file__)) + '/Kitti/training'
     dataset = Dataset(train_path)
-
 
     params = {'batch_size': batch_size,
               'shuffle': True,
@@ -29,20 +29,37 @@ def main():
 
     generator = data.DataLoader(dataset, **params)
 
-    # device = torch.device('cuda:0')
-
     my_vgg = vgg.vgg19_bn(pretrained=True)
     model = Model(features=my_vgg.features).cuda()
-
     opt_SGD = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    # conf_loss_func = nn.CrossEntropyLoss().cuda()
     conf_loss_func = nn.CrossEntropyLoss().cuda()
     dim_loss_func = nn.MSELoss().cuda()
     orient_loss_func = OrientationLoss
 
+    # load any previous weights
+    model_path = os.path.abspath(os.path.dirname(__file__)) + '/weights/'
+    latest_model = None
+    first_epoch = 0
+    if not os.path.isdir(model_path):
+        os.mkdir(model_path)
+    else:
+        latest_model = [x for x in sorted(os.listdir(model_path)) if x.endswith('.pkl')][-1]
+
+    if latest_model is not None:
+        checkpoint = torch.load(model_path + latest_model)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        opt_SGD.load_state_dict(checkpoint['optimizer_state_dict'])
+        first_epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
+
+        print 'Found previous checkpoint: %s at epoch %s'%(latest_model, first_epoch)
+        print 'Resuming training....'
+
+
+
     total_num_batches = int(len(dataset) / batch_size)
 
-    for epoch in range(epochs):
+    for epoch in range(first_epoch, epochs):
         curr_batch = 0
         passes = 0
         for local_batch, local_labels in generator:
@@ -54,20 +71,19 @@ def main():
             local_batch=local_batch.float().cuda()
             [orient, conf, dim] = model(local_batch)
 
-            # orient_loss = orient_loss_func(orient, truth_orient, truth_conf)
-            # dim_loss = dim_loss_func(dim, truth_dim)
+            orient_loss = orient_loss_func(orient, truth_orient, truth_conf)
+            dim_loss = dim_loss_func(dim, truth_dim)
 
             truth_conf = torch.max(truth_conf, dim=1)[1]
             conf_loss = conf_loss_func(conf, truth_conf)
 
-            # first, get only confidence to converge
-            # loss_theta = conf_loss + orient_loss * w
-            # loss = alpha * dim_loss + loss_theta
-            loss = conf_loss
+            loss_theta = conf_loss + w * orient_loss
+            loss = alpha * dim_loss + loss_theta
 
             opt_SGD.zero_grad()
             loss.backward()
             opt_SGD.step()
+
 
             if passes % 10 == 0:
                 print "--- epoch %s | batch %s/%s --- [loss: %s]" %(epoch, curr_batch, total_num_batches, loss.item())
@@ -76,23 +92,19 @@ def main():
             passes += 1
             curr_batch += 1
 
-        name = os.path.abspath(os.path.dirname(__file__)) + '/weights/'
-        name += 'epoch_%s.pkl' % epoch
-        print "===================="
-        print "Done with epoch %s!" % epoch
-        print "Saving weights as %s ..." % name
-        torch.save(model.state_dict(),name)
-        print "===================="
-
-
-
-
-
-
-
-
-
-
+        # save after every 10 epochs
+        if epoch % 10 == 0:
+            name = model_path + 'epoch_%s.pkl' % epoch
+            print "===================="
+            print "Done with epoch %s!" % epoch
+            print "Saving weights as %s ..." % name
+            torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': opt_SGD.state_dict(),
+                    'loss': loss
+                    }, name)
+            print "===================="
 
 if __name__=='__main__':
     main()
