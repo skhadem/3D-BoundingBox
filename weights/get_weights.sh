13ba431a167ecfd7fc4ff05d48974266aecc59a2
skhadem <soroush.khadem@colorado.edu>
2019-03-04 07:53:46 -0500

    adding script to get weights, resolves #1

---
+++ weights/get_weights.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yEiquJg9inIFgR3F-N5Z3DbFnXJ0aXmA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1yEiquJg9inIFgR3F-N5Z3DbFnXJ0aXmA" -O epoch_10.pkl && rm -rf /tmp/cookies.txt
